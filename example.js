42ad9acf5bbbe9f01a18de6016bd906e5fe66175
Siim Tiilen <siim.tiilen@tptlive.ee>
2021-10-19 09:21:57 +0300

    fix lint errors

--- example.js
+++ example.js
@@ -1,15 +1,15 @@
-const repeat = require("./repeate");
+const repeat = require('./repeate');
 
-console.log('repeat("a", 5): ', repeat("a", 5));
-console.log("repeat(\"a\", 0): ", repeat('a', 0));
+console.log('repeat("a", 5): ', repeat('a', 5));
+console.log('repeat("a", 0): ', repeat('a', 0));
 
-console.log(`repeat("testme ", 2): `, repeat("testme", 2));
+console.log('repeat("testme ", 2): ', repeat('testme', 2));
 
 try {
-    var v = repeat("x", undefined);
+  const v = repeat('x', undefined);
 
-    console.log('repeat("x", undefined):', v);
+  console.log('repeat("x", undefined):', v);
 } catch (e) {
-    console.log(' ---- error was thrwn when calling: repeat("x", undefined) ---- ')
-    console.error(e);
+  console.log(' ---- error was thrwn when calling: repeat("x", undefined) ---- ');
+  console.error(e);
 }
--- repeate.js
+++ repeate.js
@@ -1,16 +1,16 @@
 /**
  * Returns a String value that is made from count copies appended together. If count is 0,
  * the empty string is returned.
- * 
+ *
  * @param str   string to repeate
  * @param count number of copies to append
  */
 function repeate(str, count) {
-    if (Number.isNaN(+count)) {
-        throw new Error("count argument is not a number");
-    }
+  if (Number.isNaN(+count)) {
+    throw new Error('count argument is not a number');
+  }
 
-    return String(str).repeat(count);
+  return String(str).repeat(count);
 }
 
 module.exports = repeate;
